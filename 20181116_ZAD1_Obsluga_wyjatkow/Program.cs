04d789bbec9b8bd910f370b821d44f7f1b14697c
Marcin Cukrowski <marcin.cukrowski@balluff.pl>
2018-11-16 10:22:59 +0100

    obsluga bledow

--- 20181116_ZAD1_Obsluga_wyjatkow/Program.cs
+++ 20181116_ZAD1_Obsluga_wyjatkow/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace _20181116_ZAD1_Obsluga_wyjatkow
 {
@@ -10,6 +11,34 @@ namespace _20181116_ZAD1_Obsluga_wyjatkow
     {
         static void Main(string[] args)
         {
+            string path = null;
+
+            if(args.Length > 0 )
+            {
+                path = args[0];
+            }
+
+            Console.Write($"Podaj sciezke dostepu:{path}");
+            path = Console.ReadLine();
+
+            try
+            {
+                FileSystemWatcher folder = new FileSystemWatcher(path);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Exception!{ex.Message}");               
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Exception!{ex.Message}");
+            }
+            catch (PathTooLongException ex)
+            {
+                Console.WriteLine($"Exception!{ex.Message}");
+            }
+           
+            Console.ReadKey();
         }
     }
 }
